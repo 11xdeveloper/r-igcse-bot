a0bfbc1ab664ae2c41663edee7a2d269764e99ff
11xdeveloper <151532509+11xdeveloper@users.noreply.github.com>
2025-03-25 02:51:01 +0500

    setup: discord.js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { Client, Events } from "discord.js";
+
+const client = new Client({ intents: [] });
+
+client.once(Events.ClientReady, (readyClient) => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(process.env.BOT_TOKEN);
