e5a8b4c06f6ef6525b0c7ba200bf3cd8b90af14d
11xdeveloper <151532509+11xdeveloper@users.noreply.github.com>
2025-04-08 23:57:17 +0500

    feat: remove old prisma & redis schemas (features will be re-implemented)

--- src/cron/actionRequired.ts
+++ src/cron/actionRequired.ts
@@ -1,104 +1,104 @@
-import { prisma } from "@/prisma";
-import type { Cron } from "@/cron";
-import { Colors, EmbedBuilder, type APIEmbedField } from "discord.js";
+// import { prisma } from "@/prisma";
+// import type { Cron } from "@/cron";
+// import { Colors, EmbedBuilder, type APIEmbedField } from "discord.js";
 
-export const actionRequired: Cron = {
-	// once per week
-	expression: "0 0 * * 0",
-	async execute(client, _now) {
-		const thirtyDaysAgo = new Date(Date.now() - 1000 * 60 * 60 * 24 * 30);
+// export const actionRequired: Cron = {
+// 	// once per week
+// 	expression: "0 0 * * 0",
+// 	async execute(client, _now) {
+// 		const thirtyDaysAgo = new Date(Date.now() - 1000 * 60 * 60 * 24 * 30);
 
-		const guildsPreferences = await prisma.guildPreferences.findMany({
-			where: {
-				actionRequiredChannelId: {
-					not: null,
-				},
-			},
-		});
+// 		const guildsPreferences = await prisma.guildPreferences.findMany({
+// 			where: {
+// 				actionRequiredChannelId: {
+// 					not: null,
+// 				},
+// 			},
+// 		});
 
-		for (const guildPreferences of guildsPreferences) {
-			const guild = await client.guilds.fetch(guildPreferences.guildId);
+// 		for (const guildPreferences of guildsPreferences) {
+// 			const guild = await client.guilds.fetch(guildPreferences.guildId);
 
-			if (!guild || !guildPreferences.actionRequiredChannelId) {
-				continue;
-			}
+// 			if (!guild || !guildPreferences.actionRequiredChannelId) {
+// 				continue;
+// 			}
 
-			const actionRequiredChannel = await guild.channels.fetch(
-				guildPreferences.actionRequiredChannelId,
-			);
+// 			const actionRequiredChannel = await guild.channels.fetch(
+// 				guildPreferences.actionRequiredChannelId,
+// 			);
 
-			if (!actionRequiredChannel || !actionRequiredChannel.isTextBased()) {
-				continue;
-			}
+// 			if (!actionRequiredChannel || !actionRequiredChannel.isTextBased()) {
+// 				continue;
+// 			}
 
-			const punishmentsAggregate = await prisma.punishment.groupBy({
-				by: ["actionAgainst"],
-				where: {
-					guildId: guild.id,
-				},
-				_sum: {
-					points: true,
-				},
-				_max: {
-					when: true,
-				},
-			});
+// 			const punishmentsAggregate = await prisma.punishment.groupBy({
+// 				by: ["actionAgainst"],
+// 				where: {
+// 					guildId: guild.id,
+// 				},
+// 				_sum: {
+// 					points: true,
+// 				},
+// 				_max: {
+// 					when: true,
+// 				},
+// 			});
 
-			const filteredPunishments = punishmentsAggregate
-				.map((p) => ({
-					actionAgainst: p.actionAgainst,
-					totalPoints: p._sum.points || 0,
-					lastPunishment: p._max.when,
-				}))
-				.filter(
-					(p) =>
-						p.totalPoints >= 10 &&
-						p.lastPunishment &&
-						p.lastPunishment >= thirtyDaysAgo,
-				)
-				.sort((a, b) => b.totalPoints - a.totalPoints);
+// 			const filteredPunishments = punishmentsAggregate
+// 				.map((p) => ({
+// 					actionAgainst: p.actionAgainst,
+// 					totalPoints: p._sum.points || 0,
+// 					lastPunishment: p._max.when,
+// 				}))
+// 				.filter(
+// 					(p) =>
+// 						p.totalPoints >= 10 &&
+// 						p.lastPunishment &&
+// 						p.lastPunishment >= thirtyDaysAgo,
+// 				)
+// 				.sort((a, b) => b.totalPoints - a.totalPoints);
 
-			const fields: APIEmbedField[] = [];
+// 			const fields: APIEmbedField[] = [];
 
-			for (const userPunishments of filteredPunishments) {
-				const member = await guild.members
-					.fetch(userPunishments.actionAgainst)
-					.catch(() => null);
+// 			for (const userPunishments of filteredPunishments) {
+// 				const member = await guild.members
+// 					.fetch(userPunishments.actionAgainst)
+// 					.catch(() => null);
 
-				if (!member) {
-					continue;
-				}
+// 				if (!member) {
+// 					continue;
+// 				}
 
-				fields.push({
-					name: `${member.user.tag} (${member.id})`,
-					value: `Total Points: ${userPunishments.totalPoints}`,
-				});
-			}
+// 				fields.push({
+// 					name: `${member.user.tag} (${member.id})`,
+// 					value: `Total Points: ${userPunishments.totalPoints}`,
+// 				});
+// 			}
 
-			if (fields.length === 0) {
-				continue;
-			}
+// 			if (fields.length === 0) {
+// 				continue;
+// 			}
 
-			const chunks = Array.from(
-				{ length: Math.ceil(fields.length / 25) },
-				(_, i) => fields.slice(i * 25, i * 25 + 25),
-			);
+// 			const chunks = Array.from(
+// 				{ length: Math.ceil(fields.length / 25) },
+// 				(_, i) => fields.slice(i * 25, i * 25 + 25),
+// 			);
 
-			const embeds = chunks.map((chunk) =>
-				new EmbedBuilder()
-					.setTitle("Infraction Points Leaderboard")
-					.setDescription(
-						"The following users have accumulated 10 or more infraction points.",
-					)
-					.setColor(Colors.DarkNavy)
-					.addFields(chunk),
-			);
+// 			const embeds = chunks.map((chunk) =>
+// 				new EmbedBuilder()
+// 					.setTitle("Infraction Points Leaderboard")
+// 					.setDescription(
+// 						"The following users have accumulated 10 or more infraction points.",
+// 					)
+// 					.setColor(Colors.DarkNavy)
+// 					.addFields(chunk),
+// 			);
 
-			await actionRequiredChannel
-				.send({
-					embeds,
-				})
-				.catch(console.error);
-		}
-	},
-};
+// 			await actionRequiredChannel
+// 				.send({
+// 					embeds,
+// 				})
+// 				.catch(console.error);
+// 		}
+// 	},
+// };
--- src/redis/createIndexes.ts
+++ src/redis/createIndexes.ts
@@ -1,12 +1,5 @@
 import { createClient } from "redis";
 import { GuildPreferencesRepository } from "./guildPreferences";
-import { StickyMessageRepository } from "./stickyMessage";
-import { ModPingRepository } from "./modPing";
-import { PracticeQuestionRepository } from "./question";
-import { UserRepository } from "./user";
-import { ButtonInteractionRepository } from "./buttonInteraction";
-import { KeywordRepository } from "./keyword";
-
 export const redis = createClient({
 	url: process.env.REDIS_URL,
 });
@@ -14,19 +7,7 @@ export const redis = createClient({
 await redis.connect();
 
 const GuildPreferencesCache = new GuildPreferencesRepository(redis);
-const StickyMessageCache = new StickyMessageRepository(redis);
-const ModPingCache = new ModPingRepository(redis);
-const PracticeQuestionCache = new PracticeQuestionRepository(redis);
-const UserCache = new UserRepository(redis);
-const ButtonInteractionCache = new ButtonInteractionRepository(redis);
-const KeywordCache = new KeywordRepository(redis);
 
 await GuildPreferencesCache.createIndex();
-await StickyMessageCache.createIndex();
-await ModPingCache.createIndex();
-await PracticeQuestionCache.createIndex();
-await UserCache.createIndex();
-await ButtonInteractionCache.createIndex();
-await KeywordCache.createIndex();
 
 await redis.disconnect();
